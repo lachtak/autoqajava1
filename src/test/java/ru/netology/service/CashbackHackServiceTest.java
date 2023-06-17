e9f20a0add9dac243232b496797572adacb9ba6c
lachtak <lazynewocean@gmail.com>
2023-06-17 19:33:05 +0300

    juni4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,19 +1,30 @@
 package ru.netology.service;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class CashbackHackServiceTest {
 
     @Test
     public void testRemain() {
         CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        Assert.assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void testRemain2() {
+        CashbackHackService service = new CashbackHackService();
         int amount = 4800;
 
         int actual = service.remain(amount);
         int expected = 200;
 
-        Assert.assertEquals(actual, expected);
-
+        Assert.assertEquals(expected, actual);
     }
 }

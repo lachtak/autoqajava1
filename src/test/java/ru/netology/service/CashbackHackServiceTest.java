e392c1fb9acd30d9983a7ba2e8a4d81deaf7889a
lachtak <lazynewocean@gmail.com>
2023-06-18 14:40:43 +0300

    testNG CI crash

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -16,4 +16,17 @@ public class CashbackHackServiceTest {
         Assert.assertEquals(actual, expected);
 
     }
+
+    @Test
+    public void testRemain2() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        Assert.assertEquals(actual, expected);
+
+    }
+
 }
